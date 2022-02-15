570bffe9a75cfb92f0a6bc392638e9b89f48d7c7
oleg.ilechko <oleg.ilechko@vitechteam.com>
2022-02-15 23:50:05 +0200

    tmp

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:user_list/button_counter.dart';
-import 'package:user_list/user_list.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return UserList();
+    return MaterialApp(
+      title: "User List",
+      home: Scaffold(
+        appBar: AppBar(title: Text("My first app"),),
+        body: Center(child: ButtonCounter(defaultCount: 5,)),
+      ),
+    );
   }
 }
